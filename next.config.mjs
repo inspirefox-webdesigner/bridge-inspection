fdcb5d719c112dd645cc9fd36440aad7446704f6
betacoder7 <webdesigner2502@gmail.com>
2026-05-14 15:50:56 +0530

    nextjs,config change

--- next.config.mjs
+++ next.config.mjs
@@ -11,13 +11,15 @@
 
 
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/bridge-inspection",
-  assetPrefix: "/bridge-inspection/",
+  basePath: isProd ? "/bridge-inspection" : "",
+  assetPrefix: isProd ? "/bridge-inspection/" : "",
 };
 
 export default nextConfig;
