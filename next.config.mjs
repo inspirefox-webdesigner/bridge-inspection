b9ea1a7441a419a5a6b01570d57c1ef2772fa81a
betacoder7 <webdesigner2502@gmail.com>
2026-05-14 15:47:06 +0530

    Add repo name

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   output: "export",
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: "/bridge-inspection",
+  assetPrefix: "/bridge-inspection/",
 };
 
 export default nextConfig;
